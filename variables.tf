a3644d37d59396f11484f5b8332e2f44329c44ef
Jagan_Gudela <64357570+JacksPro46@users.noreply.github.com>
2024-04-03 12:28:39 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "resource_group" {
+  type = string
+  description = "This is name of rg"
+}
+
+variable "location" {
+  type = string
+  description = "This is name of location"
+}
+
+variable "aks_name" {
+  type = string
+  description = "Defining AKS Cluster name"
+}
+
+variable "aks_node" {
+  type = string
+}
+
+variable "" {
+  
+}
